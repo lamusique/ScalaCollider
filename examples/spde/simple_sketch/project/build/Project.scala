fa228f90000926a7e3fd61b9a328123966688c5c
Sciss <contact@sciss.de>
2010-08-24 23:43:12 +0100

    fixing completion actions for SynthDef. adding spde example (rather cheesy one)

---
+++ examples/spde/simple_sketch/project/build/Project.scala
@@ -0,0 +1,12 @@
+import sbt._
+import spde._
+
+class Project(info: ProjectInfo) extends DefaultSpdeProject(info) 
+  with extract.BasicSelfExtractingProject   // not needed after extraction
+  with LocalLauncherProject                 // ditto
+{
+  /** Tasks to run after extraction, not needed after */
+  override def installActions = update.name :: sbtScript.name :: "run" :: Nil
+
+   val scalaCollider = "de.sciss" %% "scalacollider" % "0.20"
+}
---
+++ examples/spde/simple_sketch/project/plugins/Plugins.scala
@@ -0,0 +1,7 @@
+import sbt._
+
+class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
+  val databinder_repo = "Databinder Repository" at "http://databinder.net/repo"
+	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.2"
+	val extract = "org.scala-tools.sbt" % "installer-plugin" % "0.3.0"
+}
